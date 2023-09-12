258d19f20f8adbf119085f53e1b97608e30a82e9
edstell <edward.j.stell@gmail.com>
2023-09-12 10:26:27 +0100

    remove transaction error interface

--- transaction_error.go
+++ transaction_error.go
@@ -5,45 +5,35 @@ import (
 	"fmt"
 )
 
-type TransactionError interface {
-	error
-	RawError() interface{}
-	xx_isTransactionError()
-}
-
-type transactionError struct {
+type TransactionError struct {
 	cause error
 	raw   interface{}
 }
 
-var _ TransactionError = &transactionError{}
-
-func (v *transactionError) Error() string {
+func (v *TransactionError) Error() string {
 	return v.cause.Error()
 }
 
-func (v *transactionError) Unwrap() error {
+func (v *TransactionError) Unwrap() error {
 	return v.cause
 }
 
-func (v *transactionError) Cause() error {
+func (v *TransactionError) Cause() error {
 	return v.cause
 }
 
-func (v *transactionError) RawError() interface{} {
+func (v *TransactionError) RawError() interface{} {
 	return v.raw
 }
 
-func (transactionError) xx_isTransactionError() {}
-
-func ParseTransactionError(tx *Transaction, raw interface{}) (TransactionError, bool) {
+func ParseTransactionError(tx *Transaction, raw interface{}) (*TransactionError, bool) {
 	switch t := raw.(type) {
 	case string:
 		err, ok := parseTransactionErrorString(t)
 		if !ok {
 			return nil, false
 		}
-		return &transactionError{
+		return &TransactionError{
 			cause: err,
 			raw:   raw,
 		}, true
@@ -52,7 +42,7 @@ func ParseTransactionError(tx *Transaction, raw interface{}) (TransactionError, 
 		if !ok {
 			return nil, false
 		}
-		return &transactionError{
+		return &TransactionError{
 			cause: err,
 			raw:   raw,
 		}, true
@@ -152,7 +142,6 @@ type TransactionError_Undefined string
 func (v TransactionError_Undefined) Error() string {
 	return string(v)
 }
-func (TransactionError_Undefined) xx_isTransactionError() {}
 
 // Defined [here](https://github.com/solana-labs/solana/blob/c0c60386544ec9a9ec7119229f37386d9f070523/sdk/src/transaction/error.rs#L13).
 
@@ -160,16 +149,14 @@ func (TransactionError_Undefined) xx_isTransactionError() {}
 // does not support parallelism
 type TransactionError_AccountInUse struct{}
 
-func (TransactionError_AccountInUse) Error() string          { return "Account in use" }
-func (TransactionError_AccountInUse) xx_isTransactionError() {}
+func (TransactionError_AccountInUse) Error() string { return "Account in use" }
 
 // A `Pubkey` appears twice in the transaction's `account_keys`.  Instructions
 // can reference `Pubkey`s more than once but the message must contain a list
 // with no duplicate keys
 type TransactionError_AccountLoadedTwice struct{}
 
-func (TransactionError_AccountLoadedTwice) Error() string          { return "Account loaded twice" }
-func (TransactionError_AccountLoadedTwice) xx_isTransactionError() {}
+func (TransactionError_AccountLoadedTwice) Error() string { return "Account loaded twice" }
 
 // Attempt to debit an account but found no record of a prior credit.
 type TransactionError_AccountNotFound struct{}
@@ -177,7 +164,6 @@ type TransactionError_AccountNotFound struct{}
 func (TransactionError_AccountNotFound) Error() string {
 	return "Attempt to debit an account but found no record of a prior credit."
 }
-func (TransactionError_AccountNotFound) xx_isTransactionError() {}
 
 // Attempt to load a program that does not exist
 type TransactionError_ProgramAccountNotFound struct{}
@@ -185,14 +171,12 @@ type TransactionError_ProgramAccountNotFound struct{}
 func (TransactionError_ProgramAccountNotFound) Error() string {
 	return "Attempt to load a program that does not exist"
 }
-func (TransactionError_ProgramAccountNotFound) xx_isTransactionError() {}
 
 // The from `Pubkey` does not have sufficient balance to pay the fee to
 // schedule the transaction
 type TransactionError_InsufficientFundsForFee struct{}
 
-func (TransactionError_InsufficientFundsForFee) Error() string          { return "Insufficient funds for fee" }
-func (TransactionError_InsufficientFundsForFee) xx_isTransactionError() {}
+func (TransactionError_InsufficientFundsForFee) Error() string { return "Insufficient funds for fee" }
 
 // This account may not be used to pay transaction fees
 type TransactionError_InvalidAccountForFee struct{}
@@ -200,7 +184,6 @@ type TransactionError_InvalidAccountForFee struct{}
 func (TransactionError_InvalidAccountForFee) Error() string {
 	return "This account may not be used to pay transaction fees"
 }
-func (TransactionError_InvalidAccountForFee) xx_isTransactionError() {}
 
 // The bank has seen this transaction before. This can occur under normal
 // operation when a UDP packet is duplicated, as a user error from a client not
@@ -210,14 +193,12 @@ type TransactionError_AlreadyProcessed struct{}
 func (TransactionError_AlreadyProcessed) Error() string {
 	return "This transaction has already been processed"
 }
-func (TransactionError_AlreadyProcessed) xx_isTransactionError() {}
 
 // The bank has not seen the given `recent_blockhash` or the transaction is too
 // old and the `recent_blockhash` has been discarded.
 type TransactionError_BlockhashNotFound struct{}
 
-func (TransactionError_BlockhashNotFound) Error() string          { return "Blockhash not found" }
-func (TransactionError_BlockhashNotFound) xx_isTransactionError() {}
+func (TransactionError_BlockhashNotFound) Error() string { return "Blockhash not found" }
 
 // An error occurred while processing an instruction.
 type TransactionError_InstructionError struct {
@@ -228,14 +209,12 @@ type TransactionError_InstructionError struct {
 func (v *TransactionError_InstructionError) Error() string {
 	return fmt.Sprintf("Error processing instruction %d: %s", v.Index, v.Cause.Error())
 }
-func (v *TransactionError_InstructionError) Unwrap() error        { return v.Cause }
-func (*TransactionError_InstructionError) xx_isTransactionError() {}
+func (v *TransactionError_InstructionError) Unwrap() error { return v.Cause }
 
 // Loader call chain is too deep
 type TransactionError_CallChainTooDeep struct{}
 
-func (TransactionError_CallChainTooDeep) Error() string          { return "Loader call chain is too deep" }
-func (TransactionError_CallChainTooDeep) xx_isTransactionError() {}
+func (TransactionError_CallChainTooDeep) Error() string { return "Loader call chain is too deep" }
 
 // Transaction requires a fee but has no signature present
 type TransactionError_MissingSignatureForFee struct{}
@@ -243,7 +222,6 @@ type TransactionError_MissingSignatureForFee struct{}
 func (TransactionError_MissingSignatureForFee) Error() string {
 	return "Transaction requires a fee but has no signature present"
 }
-func (TransactionError_MissingSignatureForFee) xx_isTransactionError() {}
 
 // Transaction contains an invalid account reference
 type TransactionError_InvalidAccountIndex struct{}
@@ -251,7 +229,6 @@ type TransactionError_InvalidAccountIndex struct{}
 func (TransactionError_InvalidAccountIndex) Error() string {
 	return "Transaction contains an invalid account reference"
 }
-func (TransactionError_InvalidAccountIndex) xx_isTransactionError() {}
 
 // Transaction did not pass signature verification
 type TransactionError_SignatureFailure struct{}
@@ -259,7 +236,6 @@ type TransactionError_SignatureFailure struct{}
 func (TransactionError_SignatureFailure) Error() string {
 	return "Transaction did not pass signature verification"
 }
-func (TransactionError_SignatureFailure) xx_isTransactionError() {}
 
 // This program may not be used for executing instructions
 type TransactionError_InvalidProgramForExecution struct{}
@@ -267,7 +243,6 @@ type TransactionError_InvalidProgramForExecution struct{}
 func (TransactionError_InvalidProgramForExecution) Error() string {
 	return "This program may not be used for executing instructions"
 }
-func (TransactionError_InvalidProgramForExecution) xx_isTransactionError() {}
 
 // Transaction failed to sanitize accounts offsets correctly implies that
 // account locks are not taken for this TX, and should not be unlocked.
@@ -276,14 +251,12 @@ type TransactionError_SanitizeFailure struct{}
 func (TransactionError_SanitizeFailure) Error() string {
 	return "Transaction failed to sanitize accounts offsets correctly"
 }
-func (TransactionError_SanitizeFailure) xx_isTransactionError() {}
 
 type TransactionError_ClusterMaintenance struct{}
 
 func (TransactionError_ClusterMaintenance) Error() string {
 	return "Transactions are currently disabled due to cluster maintenance"
 }
-func (TransactionError_ClusterMaintenance) xx_isTransactionError() {}
 
 // Transaction processing left an account with an outstanding borrowed reference
 type TransactionError_AccountBorrowOutstanding struct{}
@@ -291,4 +264,3 @@ type TransactionError_AccountBorrowOutstanding struct{}
 func (TransactionError_AccountBorrowOutstanding) Error() string {
 	return "Transaction processing left an account with an outstanding borrowed reference"
 }
-func (TransactionError_AccountBorrowOutstanding) xx_isTransactionError() {}
